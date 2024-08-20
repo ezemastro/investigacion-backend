9dfad4f80c4739c338dec2ed04cb56959df87569
ezemastro <mastropietroezequiel@gmail.com>
2024-08-20 19:47:04 -0300

    parece listo (falta ver trivia)

--- app.js
+++ app.js
@@ -3,49 +3,59 @@ import dotenv from 'dotenv'
 import sqlite3 from 'sqlite3'
 import cookie from 'cookie' // https://expressjs.com/resources/middleware/cookie-parser.html
 import { hash } from './utils/hash.js'
-import { userSchema } from './zod.js'
+import { userSchema } from './utils/zod.js'
 import { check } from './utils/check.js'
+import cors from 'cors'
 dotenv.config()
 const { PORT } = process.env
 
 sqlite3.verbose()
-const db = new sqlite3.Database('./investigacion.db', {}, (err) => {
+const db = new sqlite3.Database('./investigacion.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) console.error(err.message)
 })
+db.configure('busyTimeout', 5000)
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.post('/mail', (req, res) => {
   const { email } = req.body
   const age = parseInt(req.body.age)
 
-  // if err
+  // Validar los datos
   if (!userSchema.safeParse({ email, age }).success) {
-    res.status(400).json({
+    return res.status(400).json({
       email: userSchema.shape.email.safeParse(email).success,
       age: userSchema.shape.age.safeParse(age).success
     })
-  } else {
-    const hashedEmail = hash(email)
+  }
+
+  const hashedEmail = hash(email)
+  console.log(hashedEmail)
+
+  // Serializar las operaciones en la base de datos para evitar bloqueos
+  db.serialize(() => {
     db.get('SELECT * FROM users WHERE email = ?', [hashedEmail], (err, row) => {
       if (err) {
         console.error(err.message)
-        res.status(500).send('Failed while getting email')
+        return res.status(500).send('Failed while getting email')
       }
       if (row) {
-        res.status(409).send('Email already exists')
+        return res.status(409).send('Email already exists')
       } else {
         db.run(`
           INSERT INTO users (email, age)
           VALUES (?, ?)
-          `, [email, age], function (err) {
+        `, [hashedEmail, age], function (err) {
           if (err) {
             console.error(err.message)
-            res.status(500).send('Failed while inserting email and age')
+            return res.status(500).send('Failed while inserting email and age')
           }
+
           const id = this.lastID
-          // set cookie
+          console.log(id, typeof id)
+          // Set cookies y responder
           res.setHeader('Set-Cookie',
             cookie.serialize('id', id.toString(), { httpOnly: true }) + ';' +
             cookie.serialize('email', hashedEmail, { httpOnly: true }))
@@ -53,8 +63,9 @@ app.post('/mail', (req, res) => {
         })
       }
     })
-  }
+  })
 })
+
 app.get('/check', (req, res) => {
   const checkRes = check(req, db)
   if (checkRes.email && checkRes.id) {
@@ -63,32 +74,9 @@ app.get('/check', (req, res) => {
     res.status(checkRes.status).send(checkRes.message)
   }
 })
-app.post('/survey', (req, res) => {
-  const checkRes = check(req, db)
-  if (!checkRes.email || !checkRes.id) {
-    res.status(checkRes.status).send(checkRes.message)
-  } else if (checkRes.email && checkRes.id) {
-    if (!res.body.survey) return res.status(400).send('Missing survey')
-
-    db.run(`
-      INSERT INTO survey_responses (question_id, user_id, response) 
-      VALUES (%question_id, %user_id, %response)
-    `, {
-      question_id: req.body.survey.question_id,
-      user_id: checkRes.id,
-      response: req.body.survey.response
-    }, function (err) {
-      if (err) {
-        console.error(err.message)
-        res.status(500).send('Failed while inserting survey')
-      }
-      res.status(201).send('Survey created')
-    })
-  }
-})
 app.get('/survey', (req, res) => {
   db.all(`
-    SELECT question_id, question_text, response_type FROM survey_questions
+    SELECT question_id, question_text, response_type, option_1, option_2, option_3, option_4 FROM survey_questions
     `, function (err, rows) {
     if (err) {
       console.error(err.message)
@@ -98,9 +86,31 @@ app.get('/survey', (req, res) => {
     res.status(200).send(rows.map(row => ({
       question_id: row.question_id,
       question_text: row.question_text,
-      response_type: row.response_type
+      response_type: row.response_type,
+      options: [row.option_1, row.option_2, row.option_3, row.option_4]
     })))
   })
+})
+app.post('/survey', (req, res) => {
+  const checkRes = check(req, db)
+  if (!checkRes.email || !checkRes.id) {
+    res.status(checkRes.status).send(checkRes.message)
+  } else if (checkRes.email && checkRes.id) {
+    if (!res.body.survey) return res.status(400).send('Missing survey')
+    const query = req.body.survey.map(ans => '(?, ?, ?)').join(', ')
+    const values = req.body.survey.map(ans => [ans.question_id, checkRes.id, ans.response]).flat()
+    if (!req.body.survey.question_id || !req.body.survey.response) return res.status(400).send('Missing question_id or response')
+    db.run(`
+      INSERT INTO survey_responses (question_id, user_id, response) 
+      VALUES ${query}
+    `, values, function (err) {
+      if (err) {
+        console.error(err.message)
+        res.status(500).send('Failed while inserting survey')
+      }
+      res.status(201).send('Survey created')
+    })
+  }
 })
 
 app.post('/trivia', (req, res) => {
